2fe751820a4a7e1112f0ac8a207620fc2bf7f22f
Brandon <37425390+ghalpha@users.noreply.github.com>
2025-10-14 12:51:05 -0500

    Update run.sh

--- ntfy/run.sh
+++ ntfy/run.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
+set -e
 
+echo "Starting Ntfy server..."
+
+# Create data directory if it doesn't exist
+mkdir -p /data
+
+# Start ntfy server
+# All NTFY_* environment variables are automatically picked up
+exec ntfy serve
